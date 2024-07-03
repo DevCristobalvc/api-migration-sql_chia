dbf7327280d19dfae128dee10da77d0f8064c746
Cristobal Valencia Ceron <cristobal.valencia00@usc.edu.co>
2024-07-02 22:51:03 -0500

    heelo word!!

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # api-migration-sql_chia
+
+my_flask_api/
+├── app/
+│   ├── __init__.py
+│   ├── routes.py
+├── venv/
+├── config.py
+└── run.py
---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+@main.route('/api', methods=['GET'])
+def api_root():
+    return jsonify(message="Hello, World!")
